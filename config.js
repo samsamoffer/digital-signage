377fe97acdf1710d318f38599cc17542318cfd4e
sam sam <sam.samoffer@gmail.com>
2026-05-11 15:57:42 -0400

    final

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  posterApi: "https://script.google.com/macros/s/AKfycbzp3j7K995zuK2oDMd3dxfUbBqIuFIiG5zJlmscLES81yhphilS1Q6rXQl1GB_wNQvz/exec",
+  posterApi: "https://script.google.com/macros/s/AKfycbxzgGUF1HLxpP279V8kwToU6DoB0iYSFhhJlqxWU1wiYFSQP8X8tfjb1URsImlUt7xXXw/exec",
   donorsSheet: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT_AGGk_QcIfFGJ0k-yCw5czoV6HhjKZ__4lz_iR4Lsf_RbaGH_ezJXkLWHsQ-8z-TNXqhI3j15ipjJ/pub?output=csv",
   slideInterval: 10000, // 10 seconds per poster
   refreshInterval: 60000 // 1 minute refresh

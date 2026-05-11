82cc15082b57c7e579ce395b3be15a17cd6980c5
sam sam <sam.samoffer@gmail.com>
2026-05-11 15:28:34 -0400

    changes copilot

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-  posterApi: "https://script.google.com/macros/s/YOUR_POSTER_SCRIPT_DEPLOYMENT_ID/exec",
-  donorsSheet: "https://docs.google.com/spreadsheets/d/e/YOUR_SHEET_ID/pub?output=csv",
+  posterApi: "https://script.google.com/macros/s/AKfycbzp3j7K995zuK2oDMd3dxfUbBqIuFIiG5zJlmscLES81yhphilS1Q6rXQl1GB_wNQvz/exec",
+  donorsSheet: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT_AGGk_QcIfFGJ0k-yCw5czoV6HhjKZ__4lz_iR4Lsf_RbaGH_ezJXkLWHsQ-8z-TNXqhI3j15ipjJ/pub?output=csv",
   slideInterval: 10000, // 10 seconds per poster
   refreshInterval: 60000 // 1 minute refresh
 };
